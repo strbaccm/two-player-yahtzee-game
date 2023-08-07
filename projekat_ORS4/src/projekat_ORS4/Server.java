783009cd091cddc315b87440a6c9227e561c9566
strbaccm <110546728+strbaccm@users.noreply.github.com>
2023-08-07 17:22:56 +0200

    Update Server.java

--- projekat_ORS4/src/projekat_ORS4/Server.java
+++ projekat_ORS4/src/projekat_ORS4/Server.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 public class Server {
 	private int SERVER_PORT;
 	private ServerSocket serverSocket = null;
+	//upareni igraci, lista liste koja se sastoji od dva igraca
 	private ArrayList<ArrayList<Socket>> clients = new ArrayList<ArrayList<Socket>>();
 	private ArrayList<ClientThread> threads = new ArrayList<>();
 	
@@ -31,9 +32,53 @@ public class Server {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	private void sendInfo(Socket player1, Socket player2) {
+		String name1 = getUsername(player1);
+		String name2 = getUsername(player2);
 		
-		
+		for(ClientThread thread : threads) {
+			if(thread.getSocket() == player1)
+				thread.sendMessage("You are playing with "+name2);
+			else if(thread.getSocket() == player2)
+				thread.sendMessage("You are playing with "+name1);
+		}
 	}
 	
-
+	public synchronized void addAndPair(Socket socket) {
+		if(!clients.isEmpty() && clients.get(clients.size() - 1).size() == 1) {
+			clients.get(clients.size() - 1).add(socket);
+			Socket player1 = clients.get(clients.size() - 1).get(0);
+			Socket player2 = clients.get(clients.size() - 1).get(1);
+			sendInfo(player1, player2);
+		}
+		else {
+			ArrayList<Socket> pair = new ArrayList<>();
+			pair.add(socket);
+			clients.add(pair);
+		}
+	}
+	
+	public synchronized void clientDisconnected(ClientThread client) {
+		threads.remove(client);
+		int index = -1;
+		for (int i = 0; i < clients.size(); i++) {
+			if (client.getSocket().equals(clients.get(i).get(0))
+					|| (clients.get(i).size() == 2 && client.getSocket().equals(clients.get(i).get(1)))) {
+				index = i;
+			}
+		}
+		if (index != -1) {
+			clients.remove(index);
+		}
+	}
+	
+	private String getUsername(Socket player) {
+		for (ClientThread thread : threads) {
+			if (thread.getSocket() == player)
+				return thread.getUsername();
+		}
+		return "";
+	}
 }
